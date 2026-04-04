8d1f9900969e6f566f404ac6ef590306c43bc769
Gabriel Bunny <gbunny@usc.edu>
2026-04-03 22:19:11 -0400

    Prescription Server

--- prescription_server.py
+++ prescription_server.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 #****************************************************************************
 # Purpose: Define constants and bind the UDP socket.
@@ -23,3 +24,36 @@ except KeyboardInterrupt:
     print("\nShutting down Prescription Server...")
 finally: 
     sock.close()
+
+#****************************************************************************
+#Purpose: Reads the prescriptions.txt file then stores it in a list for use 
+#****************************************************************************
+
+def createPresList():
+    presList = []
+
+    #open the prescriptions.txt file and input it into our list var, strip each line of whitespace and ignore blank lines
+    try:
+        txtFile = open("prescriptions.txt", "r")
+        for line in txtFile:
+            line = line.strip()
+            if line is False:
+                continue
+
+            #Split the read file into 4 individual segments per none empty line
+            parts = line.split()
+            if len(parts) == 4:
+                rx = {
+                    "doctor": parts[0],
+                    "patientHash": parts[1],
+                    "treatment": parts[2],
+                    "frequency": parts[3]
+                }
+                presList.append(rx) 
+
+        txtFile.close() #close out the file once done and fully appended
+    
+    except FileNotFoundError:
+        print("Error: prescriptions.txt not there.")
+        
+    return presList
